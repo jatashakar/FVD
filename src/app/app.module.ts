e58e3cbcc1afbe4573b741ea3467603b7bb5516a
jatashankar <jspatel08@gmail.com>
2018-07-30 12:31:43 +0530

    Gitacora saved 1 changed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './src/header/home/home.component';
+import { DeskTopComponent } from './src/header/desk-top/desk-top.component';
+import { AndroidAppComponent } from './src/header/android-app/android-app.component';
+import { YoutubeMp3ConverterComponent } from './src/header/youtube-mp3-converter/youtube-mp3-converter.component';
+import { YoutubeDownloaderForIOSComponent } from './src/header/youtube-downloader-for-ios/youtube-downloader-for-ios.component';
+import { FacebookVideoDownloaderComponent } from './src/header/facebook-video-downloader/facebook-video-downloader.component';
+import { AboutUsComponent } from './src/footer/about-us/about-us.component';
+import { PrivacyPolicyComponent } from './src/footer/privacy-policy/privacy-policy.component';
+import { ContactComponent } from './src/footer/contact/contact.component';
+import { DMCAComponent } from './src/footer/dmca/dmca.component';
+import { FAQComponent } from './src/footer/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DeskTopComponent,
+    AndroidAppComponent,
+    YoutubeMp3ConverterComponent,
+    YoutubeDownloaderForIOSComponent,
+    FacebookVideoDownloaderComponent,
+    AboutUsComponent,
+    PrivacyPolicyComponent,
+    ContactComponent,
+    DMCAComponent,
+    FAQComponent
   ],
   imports: [
     BrowserModule
