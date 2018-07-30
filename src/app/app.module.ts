00af455b1cd1f343eeb42587e531468c59647da9
jatashankar <jspatel08@gmail.com>
2018-07-30 14:26:32 +0530

    Gitacora saved 2 changed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatTabsModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 // import { AppComponent } from './app.component';
 import { HomeComponent } from './src/header/home/home.component';
 import { DeskTopComponent } from './src/header/desk-top/desk-top.component';
