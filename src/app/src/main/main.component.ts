7862387502a3bcd4faa38ec8abb031c9ed47be54
jatashankar <jspatel08@gmail.com>
2018-07-31 19:16:48 +0530

    Gitacora saved 2 changed files

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+//import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Alert } from 'selenium-webdriver';
 
 @Component({
