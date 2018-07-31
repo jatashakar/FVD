5b552ab96cd549aa0555c50b07051b5dbc058837
jatashankar <jspatel08@gmail.com>
2018-07-31 19:19:53 +0530

    Gitacora saved 2 changed files

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -9,12 +9,28 @@ import { Alert } from 'selenium-webdriver';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  registerForm: FormGroup;
+    submitted = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+  });
   }
-  submit() {
-console.log('hello submit');
-  }
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    alert('SUCCESS!! :-)')
+}
 }
