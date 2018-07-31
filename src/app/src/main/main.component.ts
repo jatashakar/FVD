c42d11041efdbc17ab8b2de0f24d598a31438dfb
jatashankar <jspatel08@gmail.com>
2018-07-31 17:19:41 +0530

    Gitacora saved 2 changed files

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -7,12 +7,11 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  myGroup = new FormGroup({
-    firstName: new FormControl()
- });
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
