9a5fe3ee6ca2d93c4e31f81018a292e726ede8cd
jatashankar <jspatel08@gmail.com>
2018-08-01 11:13:47 +0530

    Gitacora saved 2 changed files

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -15,12 +15,12 @@ export class MainComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
+  //  this.registerForm = this.formBuilder.group({
       // firstName: ['', Validators.required],
       // lastName: ['', Validators.required],
       // email: ['', [Validators.required, Validators.email]],
       // password: ['', [Validators.required, Validators.minLength(6)]]
-  });
+ // });
   }
   get f() {
      return this.registerForm.controls;
