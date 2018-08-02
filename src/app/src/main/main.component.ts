c14ceaa54aa1a3a89c01810ded335300f62b549b
jatashankar <jspatel08@gmail.com>
2018-08-02 15:18:39 +0530

    Gitacora saved 1 changed files

--- src/app/src/main/main.component.ts
+++ src/app/src/main/main.component.ts
@@ -10,36 +10,36 @@ import { Alert } from 'selenium-webdriver';
 })
 export class MainComponent implements OnInit {
   registerForm: FormGroup;
-    submitted = false;
+  submitted = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.registerForm = this.formBuilder.group({
     });
-   }
-checkFunForGitHub() {
-  console.log('checkFunForGitHub');
+  }
+  checkFunForGitHub() {
+    console.log('checkFunForGitHub');
 
-}
+  }
   ngOnInit() {
     console.log('Console ngOnInit');
-  //  this.registerForm = this.formBuilder.group({
-      // firstName: ['', Validators.required],
-      // lastName: ['', Validators.required],
-      // email: ['', [Validators.required, Validators.email]],
-      // password: ['', [Validators.required, Validators.minLength(6)]]
- // });
+    //  this.registerForm = this.formBuilder.group({
+    // firstName: ['', Validators.required],
+    // lastName: ['', Validators.required],
+    // email: ['', [Validators.required, Validators.email]],
+    // password: ['', [Validators.required, Validators.minLength(6)]]
+    // });
   }
   get f() {
-     return this.registerForm.controls;
-     }
+    return this.registerForm.controls;
+  }
   onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
     if (this.registerForm.invalid) {
-        return;
+      return;
     }
 
     alert('SUCCESS!! :-)');
-}
+  }
 }
